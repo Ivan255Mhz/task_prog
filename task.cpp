87036b95a8269df43da57bf320195c204986adad
Ivan_Belyaev <ivan_work_mail@mail.ru>
2025-04-13 23:50:49 +0300

    начало работы над class task --базовые конструкторы и методы

--- task.cpp
+++ task.cpp
@@ -1,7 +1,35 @@
 #include "task.hpp"
 
+task::task() = default;
 
-task::task()=default;
+task::task(const std::string &name) : name_task(name) {}
 
-task::task(const std::string &name): name_task(name){}
+task::task(const char *name) : task(std::string(name)) {}
 
+task::task(const std::string &name, const sub_task &sub) : name_task(name)
+{
+    tasks_list.push_back(sub);
+}
+
+task::task(const char *name, const sub_task &sub) : task(std::string(name), sub) {}
+
+task::task(const std::string &name, const std::string &text) : name_task(name), text_task(text) {}
+
+task::task(const char *name, const char *text) : task(std::string(name), std::string(text)) {}
+
+task::task(const std::string &name, const std::string &text, const sub_task &sub) : name_task(name), text_task(text)
+{
+    tasks_list.push_back(sub);
+}
+
+task::task(const char *name, const char *text, const sub_task &sub) : task(std::string(name), std::string(text), sub) {}
+
+const std::string task::get_name_task() const 
+{
+    return name_task;
+}
+
+const std::string task::get_text_task() const 
+{
+    return text_task;
+}
--- task.hpp
+++ task.hpp
@@ -21,11 +21,18 @@ public:
 
     task(const std::string &name, const sub_task &sub);
 
-    task(const char *name, const sub_task $sub);
+    task(const char *name, const sub_task &sub);
 
     // Шаблонный конструктор demo--
     template <class InIter>
-    task(const std::string &name, InIter begin, InIter end);
+    task(const std::string &name, InIter begin, InIter end) : name_task(name)
+    {
+        tasks_list.assign(begin, end);
+    }
+
+    template <class InIter>
+
+    task(const char *name, InIter begin, InIter end) : task(std::string(name), begin, end){}
 
     task(const std::string &name, const std::string &text);
 
@@ -37,7 +44,13 @@ public:
 
     // Шаблонный конструктор demo--
     template <class InIter>
-    task(const std::string &name, const std::string &text, InIter begin, InIter end);
+    task(const std::string &name, const std::string &text, InIter begin, InIter end) : name_task(name), text_task(text)
+    {
+        tasks_list.assign(begin, end);
+    }
+
+    template <class InIter>
+    task(const char *name, const char *text, InIter begin, InIter end) : task(std::string(name), std::string(text), begin, end) {}
 
     const std::string get_name_task() const;
 
